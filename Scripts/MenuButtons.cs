df1c9895ff710be4c7634916740e978764857ae7
thedirge0matic <thedirge0matic@gmail.com>
2023-01-30 21:02:17 +0000

    saved

--- Scripts/MenuButtons.cs
+++ Scripts/MenuButtons.cs
@@ -3,9 +3,7 @@ using System;
 
 public class MenuButtons : VBoxContainer
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
